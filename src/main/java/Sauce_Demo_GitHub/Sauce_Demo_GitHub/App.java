62a500522f377e84f6019c319d0952888f132b90
DELL <DELL@DESKTOP-0I2OPN3>
2024-07-22 22:37:57 +0530

    Sauce_Demo_GitHub framework is crated by Team Lead

---
+++ src/main/java/Sauce_Demo_GitHub/Sauce_Demo_GitHub/App.java
@@ -0,0 +1,13 @@
+package Sauce_Demo_GitHub.Sauce_Demo_GitHub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
